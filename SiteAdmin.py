93cc3fb8c6c7765ad45775450676ca5c7bbcc2c8
Michael Donnelly <MichaelVDonnelly@gmail.com>
2011-04-15 09:53:56 -0400

    Changes for version 1 of data model

---
+++ SiteAdmin.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+#
+import datetime
+import os
+from site_db import models
+from google.appengine.ext import webapp
+from google.appengine.ext import db
+from google.appengine.ext.webapp import util
+from google.appengine.dist import use_library
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
+use_library('django', '1.2')
+from google.appengine.ext.webapp import template
+
+
+class TestHandler(webapp.RequestHandler):
+    def get(self):
+        v = models.Venue(venue_name='ThinkTank',venue_contact_email='vc@thinktankcambridge.com')
+        v.venue_address = 'One Kendall Square, Cambridge, MA 02139 USA'
+        v.put()
+        g = models.Game(play_date = datetime.date(2011,4,6), 
+                        start_time = datetime.time(19), 
+                        venue = v)
+        g.put()
+        print "Hello World"
+        for ven in models.Venue.all().fetch(10):
+            for gam in ven.game_set:
+                print "Game started at %s" % (gam.start_time)
+
+class VenueHandler(webapp.RequestHandler):
+    def get(self):
+        edit_key = self.request.get('edit',None)
+        venues = models.Venue.all().fetch(100)
+        if not edit_key is None:
+            edit_venue = [v for v in venues if (("%s" % v.key()) == edit_key)][0]
+            self.response.out.write('<!-- %s -->' % (edit_venue.venue_name))
+        else:
+            edit_venue = None
+        template_values = { 'venues': venues, 'edit_key': edit_key, 'edit_venue': edit_venue }
+        path = os.path.join(os.path.dirname(__file__), 'templates/venue.html')
+        self.response.out.write(template.render(path,template_values))
+        
+    def post(self):
+        v = self.request.get('vname').strip()
+        if v:
+            venue = models.Venue(venue_name=v)
+            if self.request.get('vaddress').strip():
+                venue.venue_address = self.request.get('vaddress').strip()
+            else:
+                venue.venue_address = None
+            if self.request.get('cemail').strip():
+                venue.venue_contact_email = self.request.get('cemail').strip()
+            else:
+                venue.venue_contact_email = None
+            if self.request.get('cphone').strip():
+                venue.venue_contact_phone = self.request.get('cphone').strip()
+            else:
+                venue.venue_contact_phone = None
+            venue.put()
+        else:
+            self.error(501,'Venue Name is required but was not provided')
+        self.redirect('/admin/venue')
+
+class GameHandler(webapp.RequestHandler):
+    def get(self):
+        edit_key = self.request.get('edit',None)
+        venues = models.Venue.all().fetch(100)
+        template_values = { 'venues': venues, 'edit_key': edit_key }
+        path = os.path.join(os.path.dirname(__file__), 'templates/game.html')
+        self.response.out.write(template.render(path,template_values))
+        
+    def post(self):
+        v = self.request.get('vname').strip()
+        if v:
+            venue = models.Venue(venue_name=v)
+            if self.request.get('vaddress').strip():
+                venue.venue_address = self.request.get('vaddress').strip()
+            else:
+                venue.venue_address = None
+            if self.request.get('cemail').strip():
+                venue.venue_contact_email = self.request.get('cemail').strip()
+            else:
+                venue.venue_contact_email = None
+            if self.request.get('cphone').strip():
+                venue.venue_contact_phone = self.request.get('cphone').strip()
+            else:
+                venue.venue_contact_phone = None
+            venue.put()
+        else:
+            self.error(501,'Venue Name is required but was not provided')
+        self.redirect('/admin/venue')
+
+def main():
+    application = webapp.WSGIApplication([
+            ('/admin/test', TestHandler),
+            ('/admin/venue', VenueHandler),
+            ('/admin/game', GameHandler)
+            ],debug=True)
+    util.run_wsgi_app(application)
+
+
+if __name__ == '__main__':
+    main()
---
+++ site_db/models.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+import datetime
+from google.appengine.ext import db
+
+class Venue(db.Model):
+    def __init__(self, *args, **kwargs):
+        if not kwargs.has_key('key'):
+            kwargs['key_name'] = kwargs['venue_name']
+        super(self.__class__, self).__init__(*args, **kwargs)
+    venue_name = db.StringProperty(required=True)
+    venue_contact_email = db.EmailProperty()
+    venue_contact_phone = db.PhoneNumberProperty()
+    venue_address = db.PostalAddressProperty()
+
+class Game(db.Model):
+    def __init__(self, *args, **kwargs):
+        if not kwargs.has_key('key'):
+            v = kwargs['venue'].venue_name
+            d = kwargs['play_date']
+            k = "%s-%s" % (v,d.isoformat())
+            kwargs['key_name'] = k
+        super(self.__class__, self).__init__(*args, **kwargs)
+    play_date = db.DateProperty(required=True)
+    start_time = db.TimeProperty(required=True)
+    venue = db.ReferenceProperty(reference_class=Venue,required=True)
+    
+class Question(db.Model):
+    question_type = db.StringProperty(required=True,choices=('basic','picture','list','set','bio'))
+    question_text = db.TextProperty(required=True)
+
+class QuestionGameMap(db.Model):
+    question = db.ReferenceProperty(required=True,reference_class=Question)
+    game = db.ReferenceProperty(required=True,reference_class=Game)
+
+class Clue(db.Model):
+    question = db.ReferenceProperty(required=True,reference_class=Question)
+    clue_text = db.TextProperty(required=True)
+    order = db.IntegerProperty(required=True)
+
+class Answer(db.Model):
+    question = db.ReferenceProperty(required=True,reference_class=Question)
+    answer_text = db.ListProperty(db.Text,required=True)
+
+class Team(db.Model):
+    def __init__(self, *args, **kwargs):
+        kwargs['key_name'] = kwargs['team_name']
+        super(self.__class__, self).__init__(*args, **kwargs)
+    team_name = db.StringProperty
+    team_notes = db.TextProperty
+
+class TeamGameMap(db.Model):
+    team = db.ReferenceProperty(required=True,reference_class=Team)
+    game = db.ReferenceProperty(required=True,reference_class=Game)
+
+class bid(db.Model):
+    team_instance = db.ReferenceProperty(required=True,reference_class=TeamGameMap)
+    question = db.ReferenceProperty(required=True,reference_class=QuestionGameMap)
+    bid_value = db.IntegerProperty(required=True,choices=(1,2,3,4,5,6,7,8,10))
+    correct = db.BooleanProperty(required=True,default=False)
+
+
+                                   
+
+
+    
+
+    
