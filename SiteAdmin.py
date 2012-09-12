c166b27b369171cf8385d9d18c3fc2d9a8e96070
Michael Donnelly <MichaelVDonnelly@gmail.com>
2012-09-12 11:48:46 -0400

    Started sensible MVC base classes.  Switched runtime to python 2.7

--- SiteAdmin.py
+++ SiteAdmin.py
@@ -13,81 +13,88 @@ from google.appengine.ext import db
 from site_db import models
 
 
+class BaseHandler(webapp.RequestHandler):
+  def get_string(self,request_key,default_value=None):
+    if self.request.get(request_key).strip():
+      return self.request.get(request_key).strip()
+    else:
+      return default_value
+
+  def render_template(self,template_file,template_values):
+    template_path = os.path.join(os.path.dirname(__file__),
+                                 'templates',template_file)
+    self.response.out.write(template.render(template_path,
+                                            template_values))
+
 class DeleteHandler(webapp.RequestHandler):
-    def get(self):
-        db.Model.get(db.Key(self.request.get('edit'))).delete()
-        self.redirect(self.request.environ['HTTP_REFERER'])
+  def get(self):
+    db.Model.get(db.Key(self.request.get('edit'))).delete()
+    self.redirect(self.request.environ['HTTP_REFERER'])
 
-class VenueHandler(webapp.RequestHandler):
-    def get(self):
-        edit_key = self.request.get('edit',None)
-        venues = models.Venue.all().fetch(100)
-        if not edit_key is None:
-            edit_venue = [v for v in venues if (("%s" % v.key()) == edit_key)][0]
-        else:
-            edit_venue = None
-        template_values = { 'venues': venues, 'edit_key': edit_key, 'edit_venue': edit_venue }
-        path = os.path.join(os.path.dirname(__file__), 'templates/venue.html')
-        self.response.out.write(template.render(path,template_values))
-        
-    def post(self):
-        v = self.request.get('vname').strip()
-        if v:
-            venue = models.Venue(venue_name=v)
-            if self.request.get('vaddress').strip():
-                venue.venue_address = self.request.get('vaddress').strip()
-            else:
-                venue.venue_address = None
-            if self.request.get('cemail').strip():
-                venue.venue_contact_email = self.request.get('cemail').strip()
-            else:
-                venue.venue_contact_email = None
-            if self.request.get('cphone').strip():
-                venue.venue_contact_phone = self.request.get('cphone').strip()
-            else:
-                venue.venue_contact_phone = None
-            venue.put()
-        else:
-            self.error(501,'Venue Name is required but was not provided')
-        self.redirect('/admin/venue')
+class VenueHandler(BaseHandler):
+  def get(self):
+    edit_key = self.request.get('edit',None)
+    venues = models.Venue.all().fetch(100)
+    if not edit_key is None:
+      edit_venue = [v for v in venues
+                    if (("%s" % v.key()) == edit_key)][0]
+    else:
+      edit_venue = None
+    self.render_template('venue.html', { 'venues': venues,
+                                         'edit_key': edit_key,
+                                         'edit_venue': edit_venue })
+  def post(self):
+    v = self.get_string('vname')
+    if v:
+      venue = models.Venue(venue_name=v)
+      venue.venue_address = self.get_string('vaddress')
+      venue.venue_contact_email = self.get_string('cemail')
+      venue.venue_contact_phone = self.get_string('cphone')
+      venue.put()
+    else:
+      self.error(501,'Venue Name is required but was not provided')
+      self.redirect('/admin/venue')
 
-class GameHandler(webapp.RequestHandler):
-    def get(self):
-        edit_key = self.request.get('edit',None)
-        venues = models.Venue.all().fetch(100)
-        if not edit_key is None:
-            edit_game = models.Game.get(db.Key(edit_key))
-        else:
-            edit_game = None
-        template_values = { 'venues': venues, 'edit_key': edit_key, 'edit_game': edit_game }
-        path = os.path.join(os.path.dirname(__file__), 'templates/game.html')
-        self.response.out.write(template.render(path,template_values))
-        
+class GameHandler(BaseHandler):
+  def get(self):
+    edit_key = self.request.get('edit',None)
+    venues = models.Venue.all().fetch(100)
+    if not edit_key is None:
+      edit_game = models.Game.get(db.Key(edit_key))
+    else:
+      edit_game = None
+    self.render_template('game.html', { 'venues': venues,
+                                        'edit_key': edit_key,
+                                        'edit_game': edit_game })
     def post(self):
-        v = self.request.get('vkey')
-        d = datetime.datetime.fromtimestamp(
-            time.mktime(time.strptime(self.request.get('play-date'),'%m/%d/%Y'))).date()
-        t = datetime.datetime.fromtimestamp(
-            time.mktime(time.strptime("01/01/1970 %s" % 
-                                      self.request.get('start-time'),'%m/%d/%Y %H:%M'))).time()
-        if v and d and t:
-            venue = models.Venue.get(db.Key(v))
-            g = models.Game(play_date = d,
-                            start_time = t,
-                            venue = venue)
-            g.put()
-        else:
-            self.error(501,'Venue, Play Date and Start Time are required but was not provided')
+      v = self.request.get('vkey')
+      d = datetime.datetime.fromtimestamp(
+          time.mktime(time.strptime(
+              self.request.get('play-date'),'%m/%d/%Y'))).date()
+      t = datetime.datetime.fromtimestamp(
+          time.mktime(time.strptime("01/01/1970 %s" %
+                                    self.request.get('start-time'),
+                                    '%m/%d/%Y %H:%M'))).time()
+      if v and d and t:
+        venue = models.Venue.get(db.Key(v))
+        g = models.Game(play_date = d,
+                        start_time = t,
+                        venue = venue)
+        g.put()
+      else:
+        self.error(501,
+                   ('Venue, Play Date and Start Time '
+                    'are required but was not provided'))
         self.redirect('/admin/game')
 
 def main():
-    application = webapp.WSGIApplication([
-            ('/admin/venue', VenueHandler),
-            ('/admin/delete', DeleteHandler),
-            ('/admin/game', GameHandler)
-            ],debug=True)
-    util.run_wsgi_app(application)
+  application = webapp.WSGIApplication([
+      ('/admin/venue', VenueHandler),
+      ('/admin/delete', DeleteHandler),
+      ('/admin/game', GameHandler)
+      ],debug=True)
+  util.run_wsgi_app(application)
 
 
 if __name__ == '__main__':
-    main()
+  main()
