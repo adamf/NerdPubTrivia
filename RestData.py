24f341907dd4e257d9bf80bbc2fcf4808952d129
Michael Donnelly <MichaelVDonnelly@gmail.com>
2012-10-22 16:29:48 -0400

    sync

--- RestData.py
+++ RestData.py
@@ -1,1 +1,61 @@
-import webapp2
+from google.appengine.ext import ndb
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import util
+from site_db import models
+from site_db import properties
+
+class RESTfulAPI(webapp.RequestHandler):
+  def __init__(self, request, response):
+    super(RESTfulAPI, self).initialize(request, response)
+    self.request.path_info_pop()
+    self.requested_api = self.request.path_info_pop()
+    self.api_key = None
+    self.uri_keys = []
+    self.uri_map = {}
+
+  def _validate(self):
+    if self.requested_api != self.api_key:
+      self.abort(400, ("Requested Handler %s didn't"
+                       "match api handler %s") % (self.requested_api,
+                                                  self.api_key))
+
+  def _map_uri_keys(self):
+    for k in self.uri_keys:
+      self.uri_map[k] = self.request.path_info_pop()
+
+  def get(self):
+    self._validate()
+    self._map_uri_keys()
+    self._get()
+
+  def _get(self):
+    pass
+
+  def put(self):
+    pass
+
+class VenueAPI(RESTfulAPI):
+  def __init__(self, request, response):
+    super(VenueAPI, self).__init__(request, response)
+    self.api_key = 'venue'
+    self.uri_keys.append('key_name')
+
+  def _get(self):
+    q = models.Venue.query()
+    for v in q.fetch():
+      print v
+    v=models.Venue(display_name='ThinkTank Bistro')
+    v.key=ndb.Key('Venue','ThinkTankBistro')
+    print v
+    v.id = v.display_name
+    v.put()
+    print v.key
+
+def main():
+  application = webapp.WSGIApplication([
+      ('/data/venue.*', VenueAPI)
+      ], debug=True)
+  util.run_wsgi_app(application)
+
+if __name__ == '__main__':
+  main()
--- site_db/models.py
+++ site_db/models.py
@@ -8,7 +8,7 @@ from google.appengine.ext import ndb
 class SelfKeyModel(ndb.Model):
 
   def __format_name_for_id(self):
-    return re.sub(r'[\s\W]','')
+    return re.sub(r'[\s\W]','',self.display_name)
 
 
 class Venue(SelfKeyModel):
@@ -22,18 +22,27 @@ class Venue(SelfKeyModel):
   twitter_handle = props.TwitterProperty()
 
   def _pre_put_hook(self):
+    if self.key is None:
+      self.key = ndb.Key(self.__class__,
+                         self.__format_name_for_id())
+
     if self.id != self.__format_name_for_id(self.display_name):
       raise ValueError('Venue name can not be changed after assignment.')
 
 class Game(ndb.Model):
     play_date = ndb.DateTimeProperty(required=True)
-    game_name = ndb.StringProperty(required=True, default='Nerd Pub Trivia')
+    game_name = ndb.StringProperty(default='Nerd Pub Trivia')
     venue = ndb.KeyProperty(kind=Venue, required=True)
 
 class Question(ndb.Model):
-    question_type = db.StringProperty(required=True,choices=('basic','picture','list','set','bio'))
+    question_type = db.StringProperty(required=True,
+                                      choices=('basic',
+                                               'picture',
+                                               'list',
+                                               'set',
+                                               'bio'))
     question_text = db.TextProperty(required=True)
-    multiplier = props.MultiplierProperty(required=True,default=1)
+    multiplier = props.MultiplierProperty(default=1)
 
 class QuestionGameMap(ndb.Model):
     question = ndb.KeyProperty(required=True,kind=Question)
@@ -55,12 +64,12 @@ class Team(ndb.Model):
 
 
 
-class TeamGameMap(db.Model):
+class TeamGameMap(ndb.Model):
     team = ndb.KeyProperty(required=True,kind=Team)
     game = ndb.KeyProperty(required=True,kind=Game)
 
-class bid(db.Model):
+class bid(ndb.Model):
     team_instance = ndb.KeyProperty(required=True,kind=TeamGameMap)
     question = ndb.KeyProperty(required=True,kind=QuestionGameMap)
     bid_value = ndb.IntegerProperty(required=True,choices=(1,2,3,4,5,6,7,8,10))
-    correct = ndb.BooleanProperty(required=True,default=False)
+    correct = ndb.BooleanProperty(default=False)
